032cc95ca9658cdd2b681df4be65b9b60bdd1dd8
Wesleysoftwaredev <aghayedow@gmail.com>
2025-11-01 12:43:47 +0000

    Add files via upload

--- menubar.py
+++ menubar.py
@@ -7,7 +7,14 @@ root.geometry('400x300')
 menubar = tk.Menu(root)
 
 file_menu = tk.Menu(menubar, tearoff = 0)
-file_menu.add_command(label = 'new file')
+
+def new_file():
+    new_window = tk.Toplevel(root)
+    new_window.title('new file')
+    new_window.geometry('200x100')
+    tk.Label(new_window, text = 'this is the new window').pack(pady=20)
+
+file_menu.add_command(label = 'new file',command=new_file)
 file_menu.add_separator()
 file_menu.add_command(label = 'exit',command = root.destroy)
 
