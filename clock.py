4aeffaf48243e016edb79cd0dcc871eb46662958
Wesleysoftwaredev <aghayedow@gmail.com>
2025-11-29 12:57:14 +0000

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import time
+
+def update_time():
+    current_time = time.strftime("%H:%M:%S")
+    label.config(text = current_time)
+    root.after(1000, update_time)
+
+root = tk.Tk()
+root.title('DIGITAL CLOCK')
+
+label = tk.Label(root, font = ("Helvetica", 48), bg = 'black', fg = 'cyan')
+label.pack(padx = 20, pady = 20)
+
+
+update_time()
+root.mainloop()
